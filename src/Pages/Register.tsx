979ffbca4ba97c8761091472953c472c059cc3ff
ismailjosim <ismailjosim99@gmail.com>
2025-08-08 16:52:16 +0600

    login register page design complete

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router";
+import logoBlack from "@/assets/icons/logo.png"
+import logoWhile from "@/assets/images/site-logo.png"
+import registerBG from "@/assets/images/register.jpg"
+import { useTheme } from "../hooks/useTheme";
+import { RegisterForm } from "../components/modules/Auth/RegisterForm";
+
+
 const Register = () => {
+    const { theme } = useTheme()
     return (
-        <div>
-            This is Register component
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="bg-muted relative hidden lg:block">
+                <img
+                    src={registerBG}
+                    alt="Image"
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+            </div>
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-end">
+                    <Link to='/' className="flex items-center  gap-2 font-medium">
+                        <img src={theme === 'dark' ? logoWhile : logoBlack} alt="Site logo" />
+                    </Link>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        <RegisterForm />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
